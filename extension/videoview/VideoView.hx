2d4ff3dfd4614c532013a9016be5283af463101a
kangel <132186966+kangeluwu@users.noreply.github.com>
2024-02-23 08:38:23 +0800

    我服了爸爸

--- extension/videoview/VideoView.hx
+++ extension/videoview/VideoView.hx
@@ -1,13 +1,11 @@
 package extension.videoview;
 
-#if !android
-#error 'extension-videoview supports only in Android platform.'
+#if android
+import lime.system.JNI;
 #end
 
-import lime.system.JNI;
-
 /**
- * author: Mihai Alexandru (MAJigsaw77)
+ * author: Saw (M.A. Jigsaw)
  */
 	
 class VideoView
@@ -17,24 +15,30 @@ class VideoView
 
 	public static function playVideo(path:String = null):Void 
 	{
+		#if android
 		var _callbackFunc = JNI.createStaticMethod("extensions/videoview/VideoViewExtension", "setCallback", "(Lorg/haxe/lime/HaxeObject;)V");
 		var _playVideo = JNI.createStaticMethod("extensions/videoview/VideoViewExtension", "playVideo", "(Ljava/lang/String;)V");
 
 		_callbackFunc(new CallBack());
 		_playVideo(path);
+		#end
 	}
 }
 
 class CallBack {
-	public function new() {}
+	public function new() {
+
+	}
 
 	public function onCompletion() {
-		if (VideoView.onCompletion != null)
+		if (VideoView.onCompletion != null) {
 			VideoView.onCompletion();
+		}
 	}
 
 	public function onPrepared() {
-		if (VideoView.onPrepared != null)
+		if (VideoView.onPrepared != null) {
 			VideoView.onPrepared();
+		}
 	}
 }
