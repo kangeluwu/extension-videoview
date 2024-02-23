e1eab68ab4d256dd46b08b2aa2c074a3730eaf6f
kangel <132186966+kangeluwu@users.noreply.github.com>
2024-02-23 08:40:39 +0800

    我服了爸爸

--- dependencies/videoview/src/extensions/videoview/VideoViewExtension.java
+++ dependencies/videoview/src/extensions/videoview/VideoViewExtension.java
@@ -5,9 +5,14 @@ import android.content.Intent;
 import org.haxe.extension.Extension;
 import org.haxe.lime.HaxeObject;
 
+/**
+ * author: Saw (M.A. Jigsaw)
+ */
+
 public class VideoViewExtension extends Extension {
 
 	public static final String EXTRA_VIDEOPATH = "extensions.videoviewex.EXTRA_VIDEOPATH";
+
 	public static HaxeObject callback;
 
 	public static void playVideo(String videoPath) {
@@ -15,10 +20,12 @@ public class VideoViewExtension extends Extension {
 			Intent intent = new Intent(mainActivity, VideoViewActivity.class);
 			intent.putExtra(EXTRA_VIDEOPATH, videoPath);
 			mainActivity.startActivity(intent);
-		} catch (Exception e)
+		} catch (Exception e) {
 			Log.d("VideoViewExtension", e.toString());
+		}
 	}
 
-	public static void setCallback(final HaxeObject _callback)
+	public static void setCallback(final HaxeObject _callback) {
 		callback = _callback;
+	}
 }
